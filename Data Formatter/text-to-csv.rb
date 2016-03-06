948c1d2b7b9df9c0f016803106b9cdb24c2cde6a
Christine L. Feaster <feasterc@gmail.com>
2016-03-06 02:28:47 -0800

    Add logic to write parsed records to CSV
    Use stubs to represent a properly formatted record parsed from the text file and use the stub to write a new row/data entry into the CSV version of the report.

--- Data Formatter/text-to-csv.rb
+++ Data Formatter/text-to-csv.rb
@@ -4,13 +4,19 @@ def convert_to_csv(file_path)
   filename, dot, ext = file_path.split("/").last.rpartition(".")
   raise "Expected file extension 'txt' but saw '#{ext}' instead" unless ext == "txt"
   new_filename = File.expand_path(OUTPUT_DIR) + "/#{filename}.csv"
+  parsed_rows = parse_text(file_path)
 
-  write_csv(new_filename)
+  write_csv(parsed_rows, new_filename) unless parsed_rows.nil?
 end
 
-def write_csv(filename="new.csv")
+def write_csv(rows_array, filename="new.csv")
   raise "'#{filename}'' exists" if File.exist? filename
 
   CSV.open("#{filename}", "ab") do |csv|
+    rows_array.each { |row| csv << row }
   end
+end
+
+def parse_text(file)
+   records =[COLUMN_HEADERS, [1,2,3]]
 end
